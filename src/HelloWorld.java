5e113ab8f4477c570905ab34319a520398d134fc
wat516 <lightsout516@gmail.com>
2019-04-21 16:06:45 +0900

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,30 @@
+
+public class HelloWorld {
+	/**
+	 * Execute this program<br>
+	 * <pre>{@code}
+	 * int main(void){
+	 *  hogehoge;
+	 * }
+	 * </pre>
+	 * 
+	 * @param args
+	 * @return whether you can live in this ugly world good luck
+	 */
+	public static void main(String... args) {
+		System.out.println("Hello World!");
+		System.out.println("Second say hello!");
+		
+		//TODO ‚±‚í‚¢
+		return;
+	}
+	
+	/**
+	 * Double x
+	 * @param x
+	 * @return {@code x * x}
+	 */
+	public static int power(int x) {
+		return x * x;
+	}
+}
